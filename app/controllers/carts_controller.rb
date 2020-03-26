117e34937ae58088a7c4a57d32437bd5937629f2
Guilherme Caraciolo <gcaraciolo@gmail.com>
2020-03-26 20:59:30 -0300

    feat(cart): add delete button for individual line item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,62 +1,18 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  before_action :set_cart, only: [:show, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
-
-  # GET /carts
-  # GET /carts.json
-  def index
-    @carts = Cart.all
-  end
 
   # GET /carts/1
   # GET /carts/1.json
   def show
   end
 
-  # GET /carts/new
-  def new
-    @cart = Cart.new
-  end
-
-  # GET /carts/1/edit
-  def edit
-  end
-
-  # POST /carts
-  # POST /carts.json
-  def create
-    @cart = Cart.new(cart_params)
-
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render :show, status: :created, location: @cart }
-      else
-        format.html { render :new }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
-  def update
-    respond_to do |format|
-      if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart }
-      else
-        format.html { render :edit }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy if @cart.id == session[:cart_id]
+    @cart.destroy
     session[:cart_id] = nil
+
     respond_to do |format|
       format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
@@ -66,12 +22,7 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def cart_params
-      params.fetch(:cart, {})
+      @cart = Cart.find(session[:cart_id])
     end
 
     def invalid_cart
--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -3,31 +3,12 @@ require "application_system_test_case"
 class CartsTest < ApplicationSystemTestCase
   setup do
     @cart = carts(:one)
+    session[:cart_id] = @cart.id
   end
 
   test "visiting the index" do
     visit carts_url
     assert_selector "h1", text: "Carts"
-  end
-
-  test "creating a Cart" do
-    visit carts_url
-    click_on "New Cart"
-
-    click_on "Create Cart"
-
-    assert_text "Cart was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Cart" do
-    visit carts_url
-    click_on "Edit", match: :first
-
-    click_on "Update Cart"
-
-    assert_text "Cart was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Cart" do
