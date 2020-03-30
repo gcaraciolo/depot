c84fc2ef9219b6cfb118ee090c6b7f87ab475a96
Guilherme Caraciolo <gcaraciolo@gmail.com>
2020-03-30 20:09:57 -0300

    feat(cart): disable checkout button in checkout page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def render_if(condition, record)
         render record if condition
     end
+
+    def current_path_begins_with(pattern)
+        request.path =~ /#{pattern}*/
+    end
 end
