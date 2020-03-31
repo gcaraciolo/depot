6a603273d92e3fc32c84fc6e25810aa1876a3ede
Guilherme Caraciolo <gcaraciolo@gmail.com>
2020-03-30 22:13:27 -0300

    feat(pay_type): add rich ui for pay type form

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
     has_many :line_items, dependent: :destroy
     enum pay_type: {
         "Check"             => 0,
-        "Credit Cart"       => 1,
+        "Credit card"       => 1,
         "Purchase order"    => 2
     }
     validates :name, :address, :email, presence: true
