928928fea79a625c028f4851bda2c01d51e5eacc
Guilherme Caraciolo <gcaraciolo@gmail.com>
2020-04-01 20:07:20 -0300

    feat(payment): integrate checkout with fake payment processor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: 'Thank you for your order' }
         format.json { render :show, status: :created, location: @order }
       else
@@ -70,7 +70,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_type_params
-    if orders_params[:pay_type] == "credit card"
+    if order_params[:pay_type] == "credit card"
       params.require(:order).permit(:credit_card_number, :expiration_date)
     elsif order_params[:pay_type] == "Check"
       params.require(:order).permit(:routing_number, :account_number)
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
@@ -42,6 +44,37 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Purchase order', from: 'Pay type'
 
     assert_selector "#order_po_number"
+  end
+
+  test "place order" do
+    LineItem.delete_all
+    Order.delete_all
+
+    checkout_before_select_pay_type
+
+    select 'Check', from: 'Pay type'
+    fill_in "Routing #", with: '123456'
+    fill_in "Account #", with: '897645'
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 
   private
